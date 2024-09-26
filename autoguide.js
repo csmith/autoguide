4630a990c8d14d5caa6051a42f9e918e7456ee16
Chris Smith <chris@chameth.com>
2024-09-26 01:47:10 +0100

    Don't force OVERLAY_WS if a zone is specified

--- autoguide.js
+++ autoguide.js
@@ -1,13 +1,5 @@
 document.addEventListener('DOMContentLoaded', async () => {
     const url = new URL(window.location.href);
-    if (!url.searchParams.has("OVERLAY_WS")) {
-        // We don't want the value url-encoded because the library doesn't parse
-        // query parameters properly (sigh), so we set a placeholder then
-        // replace it with the raw string.
-        url.searchParams.set("OVERLAY_WS", "WSplaceholderWS");
-        document.location.href = url.href.replace("WSplaceholderWS", "ws://127.0.0.1:10501/ws");
-        return;
-    }
 
     async function loadZone(data) {
         document.getElementById('location').innerText = data.zoneName;
@@ -27,6 +19,15 @@ document.addEventListener('DOMContentLoaded', async () => {
             zoneName: 'Unknown zone',
         });
     } else {
+        if (!url.searchParams.has("OVERLAY_WS")) {
+            // We don't want the value url-encoded because the library doesn't parse
+            // query parameters properly (sigh), so we set a placeholder then
+            // replace it with the raw string.
+            url.searchParams.set("OVERLAY_WS", "WSplaceholderWS");
+            document.location.href = url.href.replace("WSplaceholderWS", "ws://127.0.0.1:10501/ws");
+            return;
+        }
+
         addOverlayListener('ChangeZone', loadZone);
         startOverlayEvents()
     }
