b64f1da73916a9a8051eb777599695c8482dbdc9
Chris Smith <chris@chameth.com>
2024-09-26 01:05:00 +0100

    Set OVERLAY_WS by default

--- autoguide.js
+++ autoguide.js
@@ -1,4 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const url = new URL(window.location.href);
+    if (!url.searchParams.has("OVERLAY_WS")) {
+        // We don't want the value url-encoded because the library doesn't parse
+        // query parameters properly (sigh), so we set a placeholder then
+        // replace it with the raw string.
+        url.searchParams.set("OVERLAY_WS", "WSplaceholderWS");
+        document.location.href = url.href.replace("WSplaceholderWS", "ws://127.0.0.1:10501/ws");
+        return;
+    }
+
     addOverlayListener('ChangeZone', async (data) => {
         console.log(`Change zone`, data);
 
